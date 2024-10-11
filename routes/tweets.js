85b4371468b0b08da98fb9f40c17d682b4b0ccc7
Your name <Your email>
2024-10-11 16:40:23 +0200

    heureroute

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,37 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Tweet = require('../models/tweet');
-const User = require('../models/users');
+const Tweet = require("../models/tweet");
+const User = require("../models/users");
 
 //Message posted
-router.post('/post/:token' , (req,res) => {
-    if(!req.body.message){
-        return res.json({result: false, error: "Missing fields"})
-    }
+router.post("/post/:token", (req, res) => {
+  if (!req.body.message) {
+    return res.json({ result: false, error: "Missing fields" });
+  }
 
-    User.findOne({token: req.params.token })
-    .then(data => {
-    
-     const newTweet = new Tweet({
-        message : req.body.message,
-        date: new Date(),
-        author: data
-    })
-    newTweet.save().then(newDoc => {
-        res.json({result : true, message : newDoc})
-    })
-}) })
+  User.findOne({ token: req.params.token }).then((data) => {
+    const newTweet = new Tweet({
+      message: req.body.message,
+      date: new Date(),
+      author: data,
+    });
+    newTweet.save().then((newDoc) => {
+      res.json({ result: true, message: newDoc });
+    });
+  });
+});
 
 // Messages listing
-router.get('/get' , (req,res) => {
-    Tweet.find()
-    .populate('author')
-    .then(data => {
-        res.json({result: true, data})
-    })
-})
-
+router.get("/get", (req, res) => {
+  Tweet.find()
+    .populate("author")
+    .sort({ date: -1 })
+    .then((data) => {
+      res.json({ result: true, data });
+    });
+});
 
 module.exports = router;
-
