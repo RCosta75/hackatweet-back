4c5b52289210a5eb7fbbc18d9b1b96c36bcaa972
Lina Isabel <londonohidalgo@gmail.com>
2024-10-11 17:11:26 +0200

    back

--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,7 @@ router.post('/signup', (req, res) => {
       });
 
       newUser.save().then(newDoc => {
-        res.json({ result: true, token: newDoc.token, firstname: data.firstname });
+        res.json({ result: true, token: newDoc.token });
       });
     } else {
       // User already exists in database
@@ -51,7 +51,7 @@ router.post('/signin', (req, res) => {
 
   User.findOne({ username: req.body.username }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token, firstname: data.firstname });
+      res.json({ result: true, token: data.token });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
